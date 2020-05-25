a1937fc91bbf8143898a919733c9ccf6ceedaa9c
Leonardo Mattos <leonardo.mattos@gmail.com>
2020-05-24 22:38:58 -0400

    Adding Config and Discovery

---
+++ cloud/config-server/src/test/java/com/mattos/fintech/config/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mattos.fintech.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
