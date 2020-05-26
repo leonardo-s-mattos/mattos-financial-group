63ff79d69fb8231b9591ee78df43e7e2a0792778
Leonardo Mattos <leonardo.mattos@gmail.com>
2020-05-26 15:37:48 -0400

    adding zipkin

--- cloud/api-gateway-server/src/main/java/com/mattos/fintech/gateway/ApiGatewayServerApplication.java
+++ cloud/api-gateway-server/src/main/java/com/mattos/fintech/gateway/ApiGatewayServerApplication.java
@@ -1,17 +1,75 @@
-package com.mattos.fintech.gateway;
+package com.baeldung.spring.cloud.bootstrap.gateway;
 
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClientSpecification;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import zipkin2.Span;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
+@EnableEurekaClient
+@EnableFeignClients
 public class ApiGatewayServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
 
+	@Autowired(required = false)
+	private List<RibbonClientSpecification> configurations = new ArrayList<>();
+	@Autowired
+	private EurekaClient eurekaClient;
+	@Autowired
+	private SpanMetricReporter spanMetricReporter;
+	@Autowired
+	private ZipkinProperties zipkinProperties;
+	@Value("${spring.sleuth.web.skipPattern}")
+	private String skipPattern;
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public SpringClientFactory springClientFactory() {
+		SpringClientFactory factory = new SpringClientFactory();
+		factory.setConfigurations(this.configurations);
+		return factory;
+	}
+
+	@Bean
+	public Report makeZipkinSpanReporter() {
+		return new ZipkinSpanReporter() {
+			private HttpZipkinSpanReporter delegate;
+			private String baseUrl;
+
+			@Override
+			public void report(Span span) {
+				InstanceInfo instance = eurekaClient.getNextServerFromEureka("zipkin", false);
+				if (!(baseUrl != null && instance.getHomePageUrl().equals(baseUrl))) {
+					baseUrl = instance.getHomePageUrl();
+					delegate = new HttpZipkinSpanReporter(new RestTemplate(), baseUrl, zipkinProperties.getFlushInterval(), spanMetricReporter);
+					if (!span.name.matches(skipPattern)) delegate.report(span);
+				}
+				if (!span.name.matches(skipPattern)) delegate.report(span);
+			}
+		};
+	}
 }
---
+++ cloud/zipkin-server/src/test/java/com/mattos/fintech/zipkin/ZipkinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattos.fintech.zipkin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
